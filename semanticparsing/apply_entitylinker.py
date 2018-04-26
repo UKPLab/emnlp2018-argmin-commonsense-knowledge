88cc320789793db2f64e8f57a9cdef8394f5ab60
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2018-04-26 17:04:42 +0200

    Commit Ivan's model

---
+++ semanticparsing/apply_entitylinker.py
@@ -0,0 +1,66 @@
+
+import sys, json
+import cv2
+import torch
+
+PATH_EL = "../entity-linking/"
+sys.path.insert(0, PATH_EL)
+PATH_RELEXT = "../relation-extraction/relation_extraction/"
+sys.path.insert(0, PATH_RELEXT)
+
+import click
+import tqdm
+from pycorenlp import StanfordCoreNLP
+
+from entitylinking import core as EL
+from entitylinking.core.sentence import SentenceEncoder
+
+
+corenlp = StanfordCoreNLP('http://semanticparsing:9000')
+corenlp_properties = {
+    'annotators': 'tokenize, pos, ner',
+    'outputFormat': 'json'
+}
+
+EL.candidate_retrieval.entity_linking_p['max.match.diff'] = 0
+
+EL.mention_extraction.np_parser = EL.mention_extraction.NgramNpParser(exclude_pos={".", "ORDINAL", "TIME", "PERCENT"},
+                          exclude_if_first={"WDT", "WP", "WP$", "WRB", "VBZ", "VB", "VBP"},
+                          exclude_prefix={"IN", "DT", "CC", "POS"},
+                          exclude_suffix={"IN", "DT", "CC", "JJ", "RB", "JJR", "JJS", "RBR", "RBS"},
+                          exclude_alone={"IN", "DT", "PDT", "POS", "PRP", "PRP$", "CC", "TO",
+                                         "VBZ", "VBD", "VBP", "VB", "VBG", "VBN",
+                                         "JJ", "RB", "JJR", "JJS", "RBR", "RBS"
+                                         })
+
+
+@click.command()
+@click.argument('path_to_file')
+def apply(path_to_file):
+
+    entitylinker = EL.MLLinker(path_to_model="../entity-linking/trainedmodels/FeatureModel_6.torchweights",
+                               confidence=0.01,
+                               num_candidates=3)
+
+    with open(path_to_file) as f:
+        input_data = [l.strip().split("\t") for l in f.readlines()][1:]
+
+    input_data = [[parts[i] for i in [1,2,4,5]] for parts in input_data]
+
+    output_data = []
+    for parts in tqdm.tqdm(input_data):
+        output_per_story = []
+        for s in parts:
+            sent = entitylinker.link_entities_in_sentence_obj(EL.sentence.Sentence(input_text=s))
+            sent.entities = [{k: e[k] for k in {'type', 'linkings', 'token_ids', 'poss', 'tokens', 'drop_score'}}
+                             for e in sent.entities if len(e['linkings']) > 0]
+            for e in sent.entities:
+                e['linkings'] = [(l.get('kbID'), l.get('label')) for l in e['linkings']]
+            output_per_story.append(sent)
+        output_data.append(output_per_story)
+    with open("data/train_output.json", "w") as out:
+        json.dump(output_data, out, sort_keys=True, indent=4, cls=SentenceEncoder)
+
+
+if __name__ == '__main__':
+    apply()
